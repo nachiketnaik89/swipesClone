cbb696b769e785b1880f403320341c8b68a68101
Nachiket Naik <nachiketnaik@Nachikets-MacBook-Pro.local>
2016-09-23 00:06:31 -0700

    initial commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {DataService} from '../../services/DataService';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html',
+  providers: [DataService]
+})
+
+export class HomePage {
+   
+  private currentList:any[];
+   private tab:Tabs;
+
+  constructor(public navCtrl: NavController,public dataService:DataService) { 
+      this.currentList = dataService.getData();
+      //this.currentList =[1,2,3,4,5,6,7,8];
+      this.tab = Tabs.PENDING;
+  }
+
+
+  showScheduledItems(){
+    this.currentList = this.dataService.getScheduledList();
+    this.tab = Tabs.SCHEDULE;
+  }
+
+  showPendingList(){
+    this.currentList= this.dataService.getPendingList();
+    this.tab = Tabs.PENDING;
+  }
+
+  showCompletedList(){
+    this.currentList = this.dataService.getCompletedList();
+    this.tab = Tabs.COMPLETED;
+  }
+
+  rightSwipe(item){
+    console.log("right",item);
+
+    if(this.tab === Tabs.PENDING){
+        this.dataService.addScheduleList(item);
+        this.dataService.removeFromPendingList(item);
+    }else if(this.tab === Tabs.COMPLETED){
+        this.dataService.addScheduleList(item);
+        this.dataService.removeFromCompletedList(item);
+    }
+
+  }
+  leftSwipe(item){
+    console.log("left",item);
+
+    if(this.tab === Tabs.PENDING){
+        this.dataService.addCompletedList(item);
+        this.dataService.removeFromPendingList(item);
+        this.currentList = this.dataService.getScheduledList();
+    }else if(this.tab === Tabs.SCHEDULE){
+        this.dataService.addCompletedList(item);
+        this.dataService.removeFromScheduleList(item);
+    }
+    
+  }
+
+}
+enum Tabs {SCHEDULE, PENDING, COMPLETED}
---
+++ app/services/DataService.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+
+
+export class DataService {
+    private pendingList:any[];
+    private completedList:any[];
+    private scheduledList:any[];
+  constructor() {
+      this.pendingList = [];
+      this.completedList =[];
+      this.scheduledList =[];
+      this.init();
+  }
+
+  init(){
+      this.pendingList = ['this Pending1', 'this Pending2', 'this Pending3'];
+      this.completedList =['completed1','completed2','completed3'];
+      this.scheduledList =['schedule1', 'schedule2', 'schedule3'];
+  }
+
+  getData() {
+    return this.pendingList;
+  }
+
+  getPendingList(){
+    return this.pendingList;
+  }
+
+  getCompletedList(){
+      return this.completedList;
+  }
+
+  getScheduledList(){
+      return this.scheduledList;
+  }
+
+  addCompletedList(item){
+      if(item){
+          this.completedList.push(item);
+      }
+  }
+
+  addScheduleList(item){
+        if(item){
+          this.scheduledList.push(item);
+      }
+  }
+
+  removeFromPendingList(item){
+      if(item){
+          var index = this.pendingList.indexOf(item);
+          if(index!=-1){
+              this.pendingList = this.pendingList.splice(index,1);
+          }
+      }
+  }
+  
+  removeFromScheduleList(item){
+      if(item){
+          var index = this.scheduledList.indexOf(item);
+          if(index!=-1){
+              this.scheduledList = this.scheduledList.splice(index,1);
+          }
+      }
+  }
+
+    removeFromCompletedList(item){
+      if(item){
+          var index = this.completedList.indexOf(item);
+          if(index!=-1){
+              this.completedList = this.completedList.splice(index,1);
+          }
+      }
+  }
+
+}
